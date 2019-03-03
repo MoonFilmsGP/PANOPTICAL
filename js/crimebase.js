84fbe48833d4ba15bb2c71f52c4d64b376edf1a1
MoonFilmsGP <48173904+MoonFilmsGP@users.noreply.github.com>
2019-03-03 02:58:17 -0600

    Add files via upload

---
+++ js/crimebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD2e8ePoSlL5s9aXi8wEQubSN7Thcri-ks",
+    authDomain: "crime-base.firebaseapp.com",
+    databaseURL: "https://crime-base.firebaseio.com",
+    projectId: "crime-base",
+    storageBucket: "crime-base.appspot.com",
+    messagingSenderId: "45644663374"
+  };
+  firebase.initializeApp(config);
+</script>
